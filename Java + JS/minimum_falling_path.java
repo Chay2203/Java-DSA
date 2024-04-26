b732b66cf8f3ffaca800a3f217c1c3f41e80782b
Naga Chaitanya Varma <69190057+Chay2203@users.noreply.github.com>
2024-04-26 14:23:00 +0530

    Create minimum_falling_path.java

---
+++ Java + JS/minimum_falling_path.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int minFallingPathSum(int[][] matrix) {
+        int n = matrix.length;
+
+        for (int i = 1; i < n; i++) {
+            int minVal = Integer.MAX_VALUE, secondMinVal = Integer.MAX_VALUE, minIndex = -1;
+
+            for (int j = 0; j < n; j++) {
+                if (matrix[i - 1][j] < minVal) {
+                    secondMinVal = minVal;
+                    minVal = matrix[i - 1][j];
+                    minIndex = j;
+                } else if (matrix[i - 1][j] < secondMinVal) {
+                    secondMinVal = matrix[i - 1][j];
+                }
+            }
+
+            for (int j = 0; j < n; j++) {
+                if (j != minIndex) {
+                    matrix[i][j] += minVal;
+                } else {
+                    matrix[i][j] += secondMinVal;
+                }
+            }
+        }
+
+        int minSum = Integer.MAX_VALUE;
+        for (int val : matrix[n - 1]) {
+            minSum = Math.min(minSum, val);
+        }
+
+        return minSum;
+    }
+}
